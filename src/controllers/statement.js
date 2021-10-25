2921db9ed64759cfb858cc290c4fb502f10866d0
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-10-25 16:52:40 -0300

    Get statement functionality inplemented

--- src/controllers/statement.js
+++ src/controllers/statement.js
@@ -1,3 +1,6 @@
+import connection from "../database/database.js"
+
+
 const getStatements = async (req, res) => {
 	const {
 		headers: { authorization }
@@ -5,6 +8,21 @@ const getStatements = async (req, res) => {
 	const token = authorization?.replace('Bearer ', '')
 
 	if (!token) return res.sendStatus(401)
+
+	try {
+		const statementsPromise = await connection.query(`
+			SELECT statements.* FROM statements
+			JOIN sessions
+			ON sessions."userId" = statements."userId"
+			WHERE sessions.token = $1;
+		`, [token])
+		const statementsList = statementsPromise.rows
+
+		return res.status(200).send(statementsList)
+	} catch (error) {
+		console.log(error)
+		return res.sendStatus(500)
+	}
 }
 
 export { getStatements }
