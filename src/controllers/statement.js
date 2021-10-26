0d8b64a79106e19689392eec3a1072621f14af73
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-10-26 16:51:18 -0300

    Inprove statement response

--- src/controllers/statement.js
+++ src/controllers/statement.js
@@ -11,18 +11,42 @@ const getStatements = async (req, res) => {
 
 	try {
 		const statementsPromise = await connection.query(`
-			SELECT statements.* FROM statements
+			SELECT statements.*
+			FROM statements
 			JOIN sessions
 			ON sessions."userId" = statements."userId"
 			WHERE sessions.token = $1;
 		`, [token])
 		const statementsList = statementsPromise.rows
+		const { userId } = statementsList[0]
 
-		return res.status(200).send(statementsList)
+		if (!userId) return res.status(200).send(makeStatement())
+		
+		const balancePromise = await connection.query(`
+			SELECT SUM(value) AS balance
+			FROM statements
+			WHERE "userId" = $1
+			GROUP BY "userId";
+		`, [userId])
+		const balance = balancePromise.rows[0]
+
+		return res.status(200).send(makeStatement(statementsList, balance))
+		
 	} catch (error) {
 		console.log(error)
 		return res.sendStatus(500)
 	}
 }
+
+
+const makeStatement = (statementList=[], balanceDict={}) => {
+	const info = {
+		statementList,
+		...balanceDict
+	}
+
+	return info
+}
+
 
 export { getStatements }
